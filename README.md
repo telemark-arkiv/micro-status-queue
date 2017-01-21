c82229c547a3cfe0292fa2754f2a9c11fd015323
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-01-21 12:02:25 +0100

    Changes API (major)

--- README.md
+++ README.md
@@ -1,7 +1,31 @@
 [![Build Status](https://travis-ci.org/telemark/micro-status-queue.svg?branch=master)](https://travis-ci.org/telemark/micro-status-queue)
 [![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg?style=flat)](https://github.com/feross/standard)
 # micro-status-queue
-Viser status for de ulike køene
+Shows status for the different queues.
+
+## API
+
+### **/json**
+
+Returns json of formatted data.
+
+#### GET
+
+```bash
+$ curl https://queue.status.t-fk.win/json
+```
+
+### **/html**
+
+Renders formatted data to html. 
+
+#### GET
+
+```bash
+$ curl https://queue.status.t-fk.win/html
+```
 
 ## License
 [MIT](LICENSE)
+
+![alt text](https://robots.kebabstudios.party/micro-status-queue.png "Robohash image of micro-status-queue")
--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 'use strict'
-
+const readFileSync = require('fs').readFileSync
+const marked = require('marked')
 const { parse } = require('url')
 const { get } = require('got')
 const { send } = require('micro')
-const generatePage = require('./lib/generate-page')
+const renderPage = require('./lib/render-page')
 
 module.exports = async (request, response) => {
-  const query = parse(request.url, true).query
-  const format = query ? query.format : false
+  const { pathname } = await parse(request.url, true)
   const systems = (await get('https://systems.config.tfk.allthethings.win', {json: true})).body
   const data = await Promise.all(Object.keys(systems).map((itemKey) => get(systems[itemKey].url, {json: true})))
   const results = data
     .map((response) => response.body)
     .map((site) => Object.assign(site, systems[site.systemid]))
-    .map((site) => Object.assign({name: site.name, queue: site.queue || 0}))
+    .map((site) => Object.assign({name: site.name, status: site.queue || 0}))
 
-  send(response, 200, format === 'json' ? results : generatePage(results))
+  if (pathname === '/json') {
+    send(response, 200, results)
+  } else if (pathname === '/html') {
+    send(response, 200, renderPage(results))
+  } else {
+    const readme = readFileSync('./README.md', 'utf-8')
+    const html = marked(readme)
+    send(response, 200, html)
+  }
 }
