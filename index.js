63e654c8f3964b1add19eb7b25ff33f1476a46d2
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-01-24 11:51:35 +0100

    Implements cors for /json (minor)

--- index.js
+++ index.js
@@ -8,17 +8,21 @@ const renderPage = require('./lib/render-page')
 
 module.exports = async (request, response) => {
   const { pathname } = await parse(request.url, true)
-  const systems = (await get('https://systems.config.tfk.allthethings.win', {json: true})).body
-  const data = await Promise.all(Object.keys(systems).map((itemKey) => get(systems[itemKey].url, {json: true})))
-  const results = data
-    .map((response) => response.body)
-    .map((site) => Object.assign(site, systems[site.systemid]))
-    .map((site) => Object.assign({name: site.name, status: site.queue || 0}))
+  if (pathname === '/json' || pathname === '/html') {
+    const systems = (await get('https://systems.config.tfk.allthethings.win', {json: true})).body
+    const data = await Promise.all(Object.keys(systems).map((itemKey) => get(systems[itemKey].url, {json: true})))
+    const results = data
+      .map((response) => response.body)
+      .map((site) => Object.assign(site, systems[site.systemid]))
+      .map((site) => Object.assign({name: site.name, status: site.queue || 0}))
 
-  if (pathname === '/json') {
-    send(response, 200, results)
-  } else if (pathname === '/html') {
-    send(response, 200, renderPage(results))
+    if (pathname === '/json') {
+      response.setHeader('Access-Control-Allow-Origin', '*')
+      response.setHeader('Access-Control-Allow-Methods', 'GET')
+      send(response, 200, results)
+    } else if (pathname === '/html') {
+      send(response, 200, renderPage(results))
+    }
   } else {
     const readme = readFileSync('./README.md', 'utf-8')
     const html = marked(readme)
